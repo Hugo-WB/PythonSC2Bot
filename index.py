8492bb17efbb5920fa706fe670e1447da5b2668c
hugo7171 <hugowong71@gmail.com>
2018-11-10 17:51:06 +0000

    XD

--- index.py
+++ index.py
@@ -12,8 +12,11 @@ global pylon
 class WorkerRushBot(sc2.BotAI):
 	buildLocation=None
 	blink = False
+	army = None
+	attacking = False
 	async def on_step(self, iteration):
 		# print("                             ",self.supply_used/self.workers.amount)
+		self.army = self.units(STALKER)
 		tech = await self.techCheck()
 		nexuses = self.units(NEXUS).ready
 		cheese = False
@@ -31,14 +34,17 @@ class WorkerRushBot(sc2.BotAI):
 		await self.buildArmy(tech,techChoice,techChoice2)
 		await self.researchUpgrades(tech,techChoice,techChoice2)
 		await self.findPosition()
+		await self.attack(tech)
+		await self.micro(tech)
+	#BASIC
 	async def build_workers(self):
-		for nexus in self.units(NEXUS).ready.noqueue:
-			if self.can_afford(PROBE) and self.units(PROBE).amount < 70 and self.units(PROBE).amount<(self.units(NEXUS).amount*24):
-				await self.do(nexus.train(PROBE))
-				print("Worker")
+			for nexus in self.units(NEXUS).ready.noqueue:
+				if self.can_afford(PROBE) and self.units(PROBE).amount < 70 and self.units(PROBE).amount<(self.units(NEXUS).amount*24):
+					await self.do(nexus.train(PROBE))
+					print("Worker")
 
 	async def buildPylon(self):
-		if (self.supply_left/self.supply_used)<0.2 and not self.already_pending(PYLON):
+		if (self.supply_left/self.supply_used)<0.2 and (not self.already_pending(PYLON) or (self.supply_used>120)) and not self.supply_cap == 200:
 			nexuses = self.units(NEXUS).ready
 			if nexuses.exists:
 				if self.can_afford(PYLON):
@@ -49,6 +55,50 @@ class WorkerRushBot(sc2.BotAI):
 		if self.can_afford(NEXUS) and (self.units(PROBE).amount)/(self.units(NEXUS).ready.amount)>16 and self.units(NEXUS).amount < 3 and not self.already_pending(NEXUS):
 				await self.expand_now()
 				print("tech:",tech)
+	async def getGas(self,tech,nexuses):
+		gas = 0
+		if tech ==0:
+			pass
+		elif tech == 1:
+			gas = 0.75 * self.units(NEXUS).ready.amount
+		elif tech == 2:
+			gas = 1.5 * self.units(NEXUS).ready.amount
+		elif tech >=3:
+			gas = 2 * self.units(NEXUS).ready.amount
+		while self.units(ASSIMILATOR).amount<gas:
+			if not self.can_afford(ASSIMILATOR):
+				break
+			
+			geyser = self.state.vespene_geyser.closer_than(25.0, nexuses.random).random
+			worker = self.select_build_worker(geyser.position,force = True)
+			await self.do(worker.build(ASSIMILATOR,geyser))
+
+	#MACRO
+
+	async def buildArmy(self,tech,techChoice,techChoice2):
+		gateways = self.units(GATEWAY)
+		wrokerToArmyRatio =(self.supply_used-self.workers.amount)/self.workers.amount
+		for gw in gateways:
+			if (self.can_afford(STALKER) and self.units(CYBERNETICSCORE).amount > 0) and ((self.workers.amount<35 and wrokerToArmyRatio<0.3) or (34<self.workers.amount<60 and wrokerToArmyRatio<0.4) or (self.workers.amount>59)) and (gw.noqueue):
+				print("Stalker!!!")
+				await self.do(gw.train(STALKER))
+
+	async def getProduction(self,tech,techChoice,techChoice2):
+		totalProduction = self.units(GATEWAY).amount+self.units(ROBOTICSFACILITY).amount+self.units(STARGATE).amount
+		if (totalProduction/self.units(NEXUS).amount)<3 and tech >= 2:
+			pylon = self.units(PYLON).ready.random
+			if (self.units(GATEWAY).amount/self.units(NEXUS).amount)<=2 and tech ==2 and self.can_afford(GATEWAY):
+				await self.build(GATEWAY, near=pylon)
+			if tech ==3 and techChoice == TWILIGHTCOUNCIL and (self.units(GATEWAY).amount/self.units(NEXUS).amount)<3 and self.can_afford(GATEWAY):
+				await self.build(GATEWAY, near=pylon)
+			elif tech ==3 and (self.units(techChoice).amount/self.units(NEXUS).amount)<=1 and self.can_afford(techChoice):
+				await self.build(techChoice,near=pylon)
+		else:
+			pass
+	
+
+
+	#Tech
 	async def techCheck(self):
 		if (self.units(ROBOTICSBAY).amount >0 or self.units(FLEETBEACON).amount >0):
 			# print("tech = 5")
@@ -93,61 +143,43 @@ class WorkerRushBot(sc2.BotAI):
 				await self.build(techChoice2, near=pylon)
 				print("tech = 4,",techChoice2,"is on the way")
 
-	async def buildArmy(self,tech,techChoice,techChoice2):
-		gateways = self.units(GATEWAY)
-		wrokerToArmyRatio =(self.supply_used-self.workers.amount)/self.workers.amount
-		for gw in gateways:
-			if (self.can_afford(STALKER) and self.units(CYBERNETICSCORE).amount > 0) and ((self.workers.amount<35 and wrokerToArmyRatio<0.3) or (34<self.workers.amount<60 and wrokerToArmyRatio<0.4) or (self.workers.amount>59)) and (gw.noqueue):
-				print("Stalker!!!")
-				await self.do(gw.train(STALKER))
 
-	async def getGas(self,tech,nexuses):
-		gas = 0
-		if tech ==0:
-			pass
-		elif tech == 1:
-			gas = 1 * self.units(NEXUS).ready.amount
-		elif tech == 2:
-			gas = 1.5 * self.units(NEXUS).ready.amount
-		elif tech >=3:
-			gas = 2 * self.units(NEXUS).ready.amount
-		while self.units(ASSIMILATOR).amount<gas:
-			if not self.can_afford(ASSIMILATOR):
-				break
-			
-			geyser = self.state.vespene_geyser.closer_than(25.0, nexuses.random).random
-			worker = self.select_build_worker(geyser.position,force = True)
-			await self.do(worker.build(ASSIMILATOR,geyser))
-	async def getProduction(self,tech,techChoice,techChoice2):
-		totalProduction = self.units(GATEWAY).amount+self.units(ROBOTICSFACILITY).amount+self.units(STARGATE).amount
-		if (totalProduction/self.units(NEXUS).amount)<3 and tech >= 2:
-			pylon = self.units(PYLON).ready.random
-			if (self.units(GATEWAY).amount/self.units(NEXUS).amount)<=2 and tech ==2 and self.can_afford(GATEWAY):
-				await self.build(GATEWAY, near=pylon)
-			if tech ==3 and techChoice == TWILIGHTCOUNCIL and (self.units(GATEWAY).amount/self.units(NEXUS).amount)<3 and self.can_afford(GATEWAY):
-				await self.build(GATEWAY, near=pylon)
-			elif tech ==3 and (self.units(techChoice).amount/self.units(NEXUS).amount)<=1 and self.can_afford(techChoice):
-				await self.build(techChoice,near=pylon)
-		else:
-			pass
+	
+	
 
 	async def researchUpgrades(self,tech,techChoice,techChoice2):
-		if not self.can_afford(RESEARCH_BLINK):
-			return
-		twilight = self.units(TWILIGHTCOUNCIL).ready
-		print(self.blink)
-		if (twilight.amount>0) and (self.can_afford(RESEARCH_BLINK)) and (twilight.noqueue) and (self.blink == False):
-			await self.do(twilight(RESEARCH_BLINK))
-			self.blink = True
+		if self.units(UnitTypeId.TWILIGHTCOUNCIL).ready.exists:
+			twilight = self.units(UnitTypeId.TWILIGHTCOUNCIL).ready.first
+	
+			if(self.can_afford(RESEARCH_BLINK)) and (twilight.noqueue) and (self.blink == False):
+				await self.do(twilight(AbilityId.RESEARCH_BLINK))
+				self.blink = True
+#Attack/unit controlling
+	async def attack(self,tech):
+		if self.time > 480 and (self.supply_used-self.workers.amount)>100 and self.attacking == False:
+			self.attacking = True
+			for stalker in self.army.idle:
+				await self.do(stalker.attack(self.enemy_start_locations[0]))
+		if (self.supply_used-self.workers.amount)<30:
+			self.attacking = False
+	async def micro(self,tech):
+		for unit in self.army:
+			if unit.health_percentage < 0.2:
+				if self.has_ability(AbilityId.RESEARCH_BLINK):
+					pass
+				else:
+					await self.do(unit.move(unit.position.towards(self.start_location, 4)))
 
+	#Ez life
 	async def findPosition(self):
 		target = self.start_location
 		approach_from = self.enemy_start_locations[0]
 		self.buildLocation = target.towards(approach_from,8)
 
+
 run_game(maps.get("(2)CatalystLE"), [
 	Bot(Race.Protoss, WorkerRushBot()),
 	Computer(Race.Random, Difficulty.Easy)
-], realtime=False)
+], realtime=True)
 
 	
